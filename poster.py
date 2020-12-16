1c84800195f23960d4c48ec8b6eecb702b2b16f2
nono-n1I <nono6217n@gmail.com>
2020-12-16 23:42:47 +0900

    add puttext and so on

--- poster.py
+++ poster.py
@@ -3,6 +3,7 @@ from skimage import data, draw
 from skimage import transform, util
 import numpy as np
 from skimage import filters, color
+from PIL import ImageFont, ImageDraw, Image
 
 
 # hl_color = np.array([0, 1, 0])
@@ -61,7 +62,7 @@ class SeamCarving:
         #     keep_mask=None
         # )
         out = self.kuwahara(img)
-        out = self.draw_texts(out, texts)
+        out = self.puttext(out, texts, point, font_path, font_size, color)
         self.work_ended = True
         cv2.imwrite("./img/seam.png", out)
 
@@ -76,20 +77,25 @@ class SeamCarving:
         print("done!" + '\033[1D')
 
 
-    def draw_texts(self, img, texts, x=10, y=0, r=0, g=0, b=0, font_scale=0.7, thickness=2):
+    def puttext(self, cv_image, texts, point=(30, 30), font_path='/IPAexfont00401/ipaexm.ttf', font_size=100, color=(255,0,0)):
         h, w, _ = img.shape
-        offset_x = x  # 左下の座標
-        initial_y = y
-        dy = int(img.shape[1] / 15)
-        color = (g, b, r)  # black
+        font_path = './font'+font_path
+        font = ImageFont.truetype(font_path, font_size)
 
-        texts = [texts] if type(texts) == str else texts
+        cv_rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        pil_image = Image.fromarray(cv_rgb_image)
 
+        draw = ImageDraw.Draw(pil_image)
+        dx = 0
+        dy = (h//3)//len(texts)
         for i, text in enumerate(texts):
-            offset_y = initial_y + (i+1)*dy
-            cv2.putText(img, text, (offset_x, offset_y), cv2.FONT_HERSHEY_SIMPLEX,
-                        font_scale, color, thickness, cv2.LINE_AA)
-        return img
+            repoint = (point[0]+dx, point[1]+i*dy)
+            draw.text(repoint, text, fill=color, font=font)
+
+        cv_rgb_result_image = np.asarray(pil_image)
+        cv_bgr_result_image = cv2.cvtColor(cv_rgb_result_image, cv2.COLOR_RGB2BGR)
+
+        return cv_bgr_result_image
 
 
     def kuwahara(self, img, r=30, resize=False, rate=0.5): #元画像、正方形領域の一辺、リサイズするか、リサイズする場合の比率
@@ -100,7 +106,6 @@ class SeamCarving:
         ave=(ave[:-r-1,:-r-1]+ave[r+1:,r+1:]-ave[r+1:,:-r-1]-ave[:-r-1,r+1:])/(r+1)**2 #平均値の一括計算
         var=((var[:-r-1,:-r-1]+var[r+1:,r+1:]-var[r+1:,:-r-1]-var[:-r-1,r+1:])/(r+1)**2-ave**2).sum(axis=2) #分散の一括計算
 
-    #--以下修正部分--
         def filt(i,j):
             return np.array([ave[i,j],ave[i+r,j],ave[i,j+r],ave[i+r,j+r]])[(np.array([var[i,j],var[i+r,j],var[i,j+r],var[i+r,j+r]]).argmin(axis=0).flatten(),j.flatten(),i.flatten())].reshape(w,h,_).transpose(1,0,2)
         filtered_pic = filt(*np.meshgrid(np.arange(h),np.arange(w))).astype(img.dtype) #色の決定
@@ -109,9 +114,12 @@ class SeamCarving:
 
 
 if __name__ == '__main__':
-    imgnm = input('Enter the image name! >> ')
+    imgnm = input('Enter the relative path of the image under ./img! (ex. nittc.jpg)>> ')
     texts = input('Enter a catchphrase! >> ').split()
-    print(type(texts), texts)
+    font_size = int(input('Enter a font size as integer type! (rec = 100) >> '))
+    font_path = input('Enter the relative path of the font under ./font (ex. /IPAexfont00401/ipaexm.ttf)>> ')
+    point = tuple(map(int, input('Enter the coordinates for your catchphrase! (rec = (30, 30)) >> ').split()))
+    color = tuple(map(int, input('Enter a color for the text! (r, g, b) >> ').split()))
     img = cv2.imread('./img/'+imgnm)
     out = SeamCarving()
     out.processing()
